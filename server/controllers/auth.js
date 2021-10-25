8862cf0650844bb087c4f2b69a988388979c1272
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-10-25 19:18:04 +0200

    login back end api

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 import User from "../models/user";
 import { hashPassword, comparePassword } from "../utils/auth";
-
+import jwt from "jsonwebtoken";
 export const register = async (req, res) => {
   try {
     // console.log(req.body);
@@ -35,9 +35,33 @@ export const register = async (req, res) => {
 //login
 export const login = async (req, res) => {
   try {
-    // console.log(req.body);
-    const { name, email, password } = req.body;
     console.log(req.body);
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).exec();
+
+    if (!user) {
+      return res.status(400).send("You are not registered in our web site");
+    }
+    console.log(user);
+    //check the password
+    const mathch = await comparePassword(password, user.password);
+    //create signed jwt
+    if (mathch) {
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+      //return user and tocken without hashed password
+      user.password = undefined;
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        // secure: true, //only work on https
+      });
+      res.json(user);
+    } else {
+      return res.status(400).send("password not correct");
+    }
   } catch (err) {
     console.log(err);
     return res.status(400).send("Error. Try again.");
