e76ab1c22298278956dc9bd3bc8b489348077f6c
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-10-26 13:19:32 +0200

    32-conditional nav links

--- Client/components/TopNav.js
+++ Client/components/TopNav.js
@@ -1,14 +1,15 @@
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Menu } from "antd";
+import { Menu, SubMenu, Avatar } from "antd";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import {
   HomeOutlined,
   LoginOutlined,
   LogoutOutlined,
-  PieChartOutlined,
+  UserOutlined,
   UserAddOutlined,
+  SettingOutlined,
 } from "@ant-design/icons";
 import { toast } from "react-toastify";
 
@@ -18,6 +19,9 @@ const TopNav = () => {
   const [loading, setLoading] = useState(false);
 
   const { state, dispatch } = useContext(Context);
+  const { user } = state;
+
+  console.log(user);
   const router = useRouter();
   useEffect(() => {
     process.browser && setCurrent(window.location.pathname);
@@ -50,7 +54,7 @@ const TopNav = () => {
       >
         <Menu.Item
           key="/"
-          style={{ marginLeft: "auto" }}
+          style={{ marginRight: "auto" }}
           onClick={(e) => setCurrent(e.key)}
           icon={<HomeOutlined twoToneColor="#eb2f96" />}
         >
@@ -58,28 +62,46 @@ const TopNav = () => {
             <a>App</a>
           </Link>
         </Menu.Item>
-        <Menu.Item
-          key="/login"
-          onClick={(e) => setCurrent(e.key)}
-          icon={<LoginOutlined />}
-        >
-          <Link href="/login">
-            <a>Login</a>
-          </Link>
-        </Menu.Item>
+        {user === null && (
+          <>
+            <Menu.Item
+              key="/login"
+              onClick={(e) => setCurrent(e.key)}
+              icon={<LoginOutlined />}
+            >
+              <Link href="/login">
+                <a>Login</a>
+              </Link>
+            </Menu.Item>
 
-        <Menu.Item
-          key="/register"
-          onClick={(e) => setCurrent(e.key)}
-          icon={<UserAddOutlined />}
-        >
-          <Link href="/register">
-            <a>register</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item onClick={logout} icon={<LogoutOutlined />}>
-          Logout
-        </Menu.Item>
+            <Menu.Item
+              key="/register"
+              onClick={(e) => setCurrent(e.key)}
+              icon={<UserAddOutlined />}
+            >
+              <Link href="/register">
+                <a>register</a>
+              </Link>
+            </Menu.Item>
+          </>
+        )}
+        {user !== null && (
+          <>
+            <Menu.SubMenu
+              key="SubMenu"
+              icon={<Avatar size="large" icon={<UserOutlined />} />}
+              title={`welcome ${user.name}`}
+            >
+              <Menu.Item
+                onClick={logout}
+                icon={<LogoutOutlined />}
+                className="float-right"
+              >
+                Logout
+              </Menu.Item>
+            </Menu.SubMenu>
+          </>
+        )}
       </Menu>
     </>
   );
