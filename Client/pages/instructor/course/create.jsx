80ae649727bdfd32a6f1595968e9fa79a6bbffe5
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-11-25 23:35:28 +0200

    60. Create course form part 2

--- Client/pages/instructor/course/create.jsx
+++ Client/pages/instructor/course/create.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 import InstructorRoute from "../../../components/routes/InstructorRoute";
+import "arch-editor/dist/arch-editor.css";
+import { ArchEditor, BlockToolbar, ArchEditorProvider } from "arch-editor";
+import { Button, Select } from "antd";
+import { SaveOutlined } from "@ant-design/icons";
 
+const { Option } = Select;
 const CourseCreate = () => {
   const [values, setvalues] = useState({
     name: "",
     description: "",
     price: "9.99",
     uploading: false,
+    paid: true,
+    category: "",
     loading: false,
-    imagePreview: "",
   });
 
   const handleChange = (e) => {
@@ -19,7 +25,8 @@ const CourseCreate = () => {
     e.preventDefault();
     console.log(values);
   };
-
+  const handleImage = (e) => {};
+  const handleMultibleFiles = (e) => {};
   const courseCreateForm = () => (
     <>
       <div className="mx-auto " style={{ width: "100%" }}>
@@ -43,9 +50,93 @@ const CourseCreate = () => {
               </div>
             </div>
 
-            <button type="submit" class="btn btn-primary">
-              Submit
-            </button>
+            <div class="mb-3 form-group border  text-center">
+              <label for="exampleInputdeasription" className="form-label">
+                Description
+              </label>
+              <div className="mt-3 border">
+                <ArchEditorProvider>
+                  <BlockToolbar />
+                  <ArchEditor
+                    placeholder="Please enter course description."
+                    showInlineToolbar
+                    row={5}
+                    col={15}
+                    name="description"
+                    value={values.description}
+                    onChange={handleChange}
+                  />
+                </ArchEditorProvider>
+              </div>
+            </div>
+            <div className="form-row ">
+              <div className="col">
+                <div className="form-group">
+                  <label for="exampleInputdeasription" className="form-label">
+                    paid course or not
+                  </label>
+                  <Select
+                    // defaultValue="paid"
+                    style={{ width: "100%" }}
+                    size="large"
+                    value={values.paid}
+                    onChange={(v) =>
+                      setvalues({ ...values, paid: !values.paid })
+                    }
+                  >
+                    <Option value={true}>Paid</Option>
+                    <Option value={false}>Free</Option>
+                  </Select>
+                </div>
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="col">
+                <div className="form-group">
+                  <label className="btn btn-outline-secondary btn-block text-left">
+                    {values.loading ? "uploading" : "Image Upload"}
+                  </label>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    name="image"
+                    onChange={handleImage}
+                  ></input>
+                </div>
+
+                <div className="form-group">
+                  <label className="btn btn-outline-secondary btn-block text-left">
+                    {values.loading ? "uploading" : "Uploade multiple Files"}
+                  </label>
+                  <input
+                    type="file"
+                    accept="image/*,.pdf"
+                    onChange={handleMultibleFiles}
+                    name="documents"
+                    multiple
+                  ></input>
+                </div>
+              </div>
+            </div>
+            {/* <div className="row">
+              <div className="col"></div>
+            </div> */}
+            <div class="container my-3">
+              <div class="col-md-12 text-center">
+                <Button
+                  onClick={handleSubmit}
+                  disabled={values.loading || values.uploading}
+                  className="btn btn-primary form-text text-center"
+                  loading={values.loading}
+                  type="primary"
+                  size="large"
+                  shape="round"
+                  icon={<SaveOutlined />}
+                >
+                  {values.loading ? "Saving..." : "Save & Continue"}
+                </Button>
+              </div>
+            </div>
           </form>
         </div>
       </div>
